1807b8cadc9384d43d4bb5077b3490be387c3f75
Michael Maier <mm263@hdm-stuttgart.de>
2025-02-10 15:12:58 +0100

    fonts and navbar added

---
+++ src/app/main/main/main.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
