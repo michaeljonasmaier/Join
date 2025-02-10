009445dd8fcf8179510e2d50cbcd64128675eebe
Michael Maier <mm263@hdm-stuttgart.de>
2025-02-10 14:36:31 +0100

    firebase implemented

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"join-8ad31","appId":"1:56133074371:web:291ce568bdce5614639176","storageBucket":"join-8ad31.firebasestorage.app","apiKey":"AIzaSyC-je28q_3U0hgeAY7Eq9b1KlC6CHGX9BE","authDomain":"join-8ad31.firebaseapp.com","messagingSenderId":"56133074371"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
