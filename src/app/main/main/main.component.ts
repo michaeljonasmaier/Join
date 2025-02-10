7ce29d1e15ff97c42b5391a5ad2560f29a8fe3a9
Michael Maier <mm263@hdm-stuttgart.de>
2025-02-10 15:45:00 +0100

    header prototype added

--- src/app/main/main/main.component.ts
+++ src/app/main/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
