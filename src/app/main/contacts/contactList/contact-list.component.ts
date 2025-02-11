075347100504c5fd240b61ee26b6f2b75f57f0a9
Michael Maier <mm263@hdm-stuttgart.de>
2025-02-11 15:22:15 +0100

    contact-list sorted

--- src/app/main/contacts/contactList/contact-list.component.ts
+++ src/app/main/contacts/contactList/contact-list.component.ts
@@ -1,21 +1,43 @@
 import { Component } from '@angular/core';
 import { FirebaseContactsService } from '../../../services/firebase-contacts.service';
 import { Contact } from '../../../interfaces/contact';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.scss'
 })
 export class ContactListComponent {
-  constructor(private contactsService: FirebaseContactsService) {
+  bgColors = ["#D72638", "#F46036", "#F3A712", "#5B8E7D", "#3A86FF", "#8338EC", "#FF006E", "#2EC4B6", "#EF476F", "#06D6A0"];
+  contactList: Contact [] = [];
 
+  constructor(private contactsService: FirebaseContactsService) {
 
   }
 
   getList(): Contact[] {
     return this.contactsService.contacts;
   }
+
+  getSortedList(): Contact[]{
+    this.contactList = this.getList()
+    let sortedContactList = this.contactList.sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+    return sortedContactList;
+  }
+
+  getInitials(name: string, surname: string) {
+    let nameInitial = name.slice(0, 1);
+    let surnameInitial = surname.slice(0, 1);
+    return nameInitial + surnameInitial;
+  }
+
+  getFirstLetter(name: string){
+    return name.slice(0, 1);
+  }
+
 }
