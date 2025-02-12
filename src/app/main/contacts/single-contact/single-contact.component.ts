c12509bb9660a1bd999aead731037f31fc560124
Marii <mariiaivanova0911@gmail.com>
2025-02-12 16:43:40 +0100

    single-comment

--- src/app/main/contacts/single-contact/single-contact.component.ts
+++ src/app/main/contacts/single-contact/single-contact.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { JoinBtnComponent } from '../../../shared/join-btn/join-btn.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-single-contact',
   standalone: true,
-  imports: [JoinBtnComponent],
+  imports: [JoinBtnComponent, FormsModule],
   templateUrl: './single-contact.component.html',
   styleUrl: './single-contact.component.scss'
 })
 export class SingleContactComponent {
+  contact = {
+    name: "",
+    email: "",
+    phone: ""
+  }
   isHidden = true;
 
   closeInfo(){
@@ -25,5 +31,22 @@ export class SingleContactComponent {
     let window = document.getElementById('add-contact');
     if (!window) return;
     window.style.transform = this.isHidden ? 'translateX(150%)' : 'translateX(0)';
+  }
+  cleanInputs(){
+    this.contact = { name: '', email: '', phone: '' };
+  }
+
+  createContact(){
+    const [name, surname] = this.contact.name.split(' ');
+
+    const newContact = {
+      name: name || '',  
+      surname: surname || '',
+      email: this.contact.email,
+      phone: this.contact.phone
+    };
+
+    console.log('Создан новый контакт:', newContact);
+    this.cleanInputs();
   }
 }
