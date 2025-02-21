5cc7f54d204d76116cd5c00c3951216dbdcc498b
Marii <mariiaivanova0911@gmail.com>
2025-02-21 11:43:20 +0100

    add-task changed

--- src/app/main/addTask/add-task.component.ts
+++ src/app/main/addTask/add-task.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
 import { Task } from '../../interfaces/task';
-import { FormsModule ,ReactiveFormsModule , Validators ,FormGroup ,FormBuilder} from '@angular/forms';
+import { FormsModule ,ReactiveFormsModule , Validators ,FormGroup ,FormBuilder, FormArray} from '@angular/forms';
 
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [FormsModule ,ReactiveFormsModule ],
+  imports: [FormsModule ,ReactiveFormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
+  task: Task [] = [];
   taskForm: FormGroup;
+  //subtasks: FormArray;
+
   users = [
     { id: '1', name: 'Alice' },
     { id: '2', name: 'Bob' }
   ];
   categories = ['Work', 'Personal', 'Other'];
+
 
   constructor(private fb: FormBuilder) {
     this.taskForm = this.fb.group({
@@ -24,8 +28,8 @@ export class AddTaskComponent {
       description: [''],
       assigned: [''],
       dueDate: ['', Validators.required],
-      category: ['', Validators.required],
-      subtasks: ['']
+      category: ['' , Validators.required ],
+      subtasks: this.fb.array([])
     });
   }
 
@@ -42,4 +46,10 @@ export class AddTaskComponent {
       console.log('Task Submitted:', this.taskForm.value);
     }
   }
+ /*
+  createTask(){
+    if (this.taskForm.valid) {
+      console.log('Task Submitted:', this.taskForm.value);
+    }
+  }*/
 }
