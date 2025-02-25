d5fd3523e34d64d13c5f76d471568721ad627cdd
Michael Maier <mm263@hdm-stuttgart.de>
2025-02-25 11:09:26 +0100

    assigned to input with filter function implemented

--- src/app/main/board/board-content/edit-task/edit-task.component.ts
+++ src/app/main/board/board-content/edit-task/edit-task.component.ts
@@ -1,15 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild  } from '@angular/core';
 import { Task } from '../../../../interfaces/task';
 import { MatSelectModule, } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { GreyBackgroundComponent } from '../../../../shared/grey-background/grey-background.component';
-import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, FormGroup, FormsModule } from '@angular/forms';
+import { Contact } from '../../../../interfaces/contact';
+import { FirebaseContactsService } from '../../../../services/firebase-contacts.service';
+import { FirebaseTasksService } from '../../../../services/firebase-tasks.service';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-edit-task',
   standalone: true,
-  imports: [MatFormFieldModule, MatSelectModule, CommonModule, GreyBackgroundComponent, ReactiveFormsModule],
+  imports: [MatFormFieldModule, MatSelectModule, CommonModule, GreyBackgroundComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './edit-task.component.html',
   styleUrl: './edit-task.component.scss'
 })
@@ -21,8 +25,11 @@ export class EditTaskComponent {
   });
 
   selectedPrio: string = "Medium";
+  contactListOpened: boolean = false;
+  filteredContactList: Contact [] = [];
+  contactInputValue: string = '';
 
-  constructor() {
+  constructor(private contactService: FirebaseContactsService, private taskService: FirebaseTasksService) {
     
   }
 
@@ -30,6 +37,28 @@ export class EditTaskComponent {
     this.selectedPrio = this.task.prio;
     this.dueDateFormGroup.patchValue({ dueDate: '2010-01-01' });
     this.getDataFormat();
+  }
+
+  toggleContactList(){
+    this.contactListOpened = !this.contactListOpened;
+  }
+
+  filterList(){
+    this.filteredContactList = this.contactService.contacts;
+    if(this.contactInputValue!=''){
+      let filteredContacts = this.filteredContactList.filter(contact => {
+        let fullName = (contact.name + ' ' + contact.surname).toLowerCase();
+        return fullName.includes(this.contactInputValue);
+      });
+      this.filteredContactList = filteredContacts;
+      return this.filteredContactList
+    } else {
+      return this.filteredContactList;
+    }
+  }
+
+  getContacts(){
+    return this.filterList();
   }
 
   getDataFormat(){
@@ -50,4 +79,22 @@ export class EditTaskComponent {
     this.close.emit();
   }
 
+  assignContact(contact: Contact){
+    let index = this.task.assigned?.findIndex(assignedContact => assignedContact.id === contact.id); 
+    if(index !== undefined && index !== -1){
+      this.task.assigned?.splice(index, 1);
+    } else {
+      this.task.assigned?.push(contact);
+    }
+    this.taskService.updateTask(this.task, this.task.status);
+  }
+
+  isContactAssigned(contact: Contact){
+    let index = this.task.assigned?.findIndex(assignedContact => assignedContact.id === contact.id);
+    if(index !== undefined && index !== -1){
+      return true;
+    } else {
+      return false;
+    } 
+  }
 }
